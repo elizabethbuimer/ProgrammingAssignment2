3db29c352ae117c950467f963d3e5d72b4334c06
elizabethbuimer <elizabeth.buimer@student.uva.nl>
2015-12-27 12:28:25 +0100

    First function calculates inverse of input matrix 'x' and makes a list 'L' of objects (combinations of functions and in/output. The second function takes this list (L) as input and returns from memory or calculates the inverse  of matrix x. Part of coursera course: R-programming (Assignment 2)
    Signed-off-by: elizabethbuimer <elizabeth.buimer@student.uva.nl>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Returns the inverse of matrix x 
 
-## Write a short comment describing this function
+## makeCacheMatrix calculates the inverse of matrix x and saves it in the cache memory
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {	# Call function as follows: L<-makeCacheMatrix(x) Where x is a square invertible matrix
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y 					# Substitute old x with new input matrix 
+		inverse <<- NULL 				# Resetting inverse to NULL 
+	}
+	get <- function() x 
+	setsolve <- function(solve) inverse <<- solve	 	# Store input variable and function
+	getsolve <- function() inverse				# Return it	
+	list(set=set, get=get, 
+	     setsolve=setsolve,
+	     getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve inverses matrix x either by calculating it or by reading it from the cache memory
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(L) {						# L is the output from the makeCacheMatrix function
+      ## Return a matrix that is the inverse of original input matrix 'x'
+	inverse <- L$getsolve()
+	if(!is.null(inverse)) {
+		message("getting cached data")
+		return(inverse)
+	}
+	data <- L$get()
+	inverse<- solve(data)
+	L$setsolve(inverse)
+	inverse
 }
